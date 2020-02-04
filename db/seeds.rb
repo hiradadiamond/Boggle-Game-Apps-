810020c16353b59ec7d7a49e4f434fcdf762c6b0
Hira Kafle <hira.kafle@coalitiontechnologies.com>
2020-02-04 17:27:46 +0545

    fixed issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@ x= randompuzzle
 
  GameContent.create([{ user: '', scores:0, puzzle:'esggmicbanzeetek', validwords:'ant, eat,nice, ice, mice, man, sim,mat,mint,net,size'}])
  GameContent.create([{ user: '', scores:0, puzzle:'keteeznaecimggse', validwords:'ice, aim, mice, sim, ant,eat,name,neat,ken,tea,egg,net'}])
- GameContent.create([{ user: '', scores:0, puzzle:'avamusufnklsarei', validwords:'are, fuse,mask'}])
+ GameContent.create([{ user: '', scores:0, puzzle:'avamusufnklsarei', validwords:'are, fuse,mask,lie'}])
  GameContent.create([{ user: '', scores:0, puzzle:'cergeisbbtmcbont', validwords:'bet,bit,sim,mice, ice, see, bot, not'}])
  GameContent.create([{ user: '', scores:0, puzzle:'einsyrtolshcriyp', validwords:'slip,not,try,hot, his'}])
  GameContent.create([{ user: '', scores:0, puzzle:'heptvsleithtrnac', validwords:'cat,hat,act,sit,pet,ant,help'}])
- GameContent.create([{ user: '', scores:0, puzzle:'erbfefoverrasats', validwords:'free,rat,sat, art,ear'}])
+ GameContent.create([{ user: '', scores:0, puzzle:'erbfefoverrasats', validwords:'free,rat,sat, art,ear,see,bore'}])
  GameContent.create([{ user: '', scores:0, puzzle:'fblprieagmndhtsv', validwords:'grin, mind, pea, lean, mine, lean'}])
